0abf1e3bafd456799e8b91c25d41319ad148aafc
Nick Hamilton <nicholasham95@gmail.com>
2019-03-15 20:46:58 -0400

    Dynamically Outputting ingredients
    (Burger.JS)
    
    Now instead of hard coding the ingredients within the burger function.. i can just have the two static ingredients(bread top and bottom) and insert the transformed ingredients function within them to add the other ingredients.
    
    (Burger Builder)
    
    Now i can manage the burger ingredients within the burger builder components state.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,16 +5,26 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 /* Function that adds the ingredients that make up burger */
 
 const burger = (props) => {
+
+
+    const transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+  
+    /* it's just a way to transform an object of key value pairs into an array of burger ingredients
+
+where the value of that object is important for me to decide how many ingredients I need and the keys
+
+important for which type of ingredient I need. */
+
 return(
 
 <div className={classes.Burger}>
 <BurgerIngredient type ="bread-top"/>
-<BurgerIngredient type="cheese"/>
-<BurgerIngredient type="cheese"/>
-<BurgerIngredient type="bacon"/>
-<BurgerIngredient type="bacon"/>
-<BurgerIngredient type="salad"/>
-<BurgerIngredient type="salad"/>
+{transformedIngredients}
 <BurgerIngredient type="bread-bottom"/>
 </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,30 +1,33 @@
 import React, {Component} from 'react';
-
 import Aux from '../../hoc/Aux';
-
 import Burger from '../../components/Burger/Burger';
 
 
+
+ 
 /* Component thats holds the Burger component and the  Build Controls Component */
 class BurgerBuilder extends Component{
-    render(){
+
+
+ state = {
+  ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+  }
+}
+    render(){  
+     
+       
         return(
 
           /* used aux to return two children (Burger, Build Controls) */
           <Aux>
-            <Burger/>
+             <Burger ingredients={this.state.ingredients} />
             <div>Build Controls</div>
           </Aux>
-
-
-
-
          );
-
-
     }
-
 }
-
-
 export default BurgerBuilder;
