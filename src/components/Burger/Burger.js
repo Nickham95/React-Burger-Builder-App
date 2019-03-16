df407823236ea1511624cefa4e952c7a5a794710
Nick Hamilton <nicholasham95@gmail.com>
2019-03-16 02:15:00 -0400

    initialized ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,19 +6,30 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
+/* it's just a way to transform an object of key value pairs into an array of burger ingredients
 
-    const transformedIngredients = Object.keys( props.ingredients )
+where the value of that object is important for me to decide how many ingredients I need and the keys
+
+important for which type of ingredient I need. */
+    let transformedIngredients = Object.keys( props.ingredients )
         .map( igKey => {
             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             } );
         } )
-  
-    /* it's just a way to transform an object of key value pairs into an array of burger ingredients
 
-where the value of that object is important for me to decide how many ingredients I need and the keys
 
-important for which type of ingredient I need. */
+
+        .reduce((arr,el)=> {
+            return  arr.concat(el)}, []); 
+
+
+            // if there is no ingredients display paragraph
+            if(transformedIngredients.length ===0){
+                transformedIngredients = <p>Please Start Adding Ingredients</p>
+            }
+       
+    
 
 return(
 
