cd625c2213b6729cced7752d25e062cf0f68850a
Nick Hamilton <nicholasham95@gmail.com>
2019-03-17 09:02:31 -0400

    Burger 2.0.4 (Order button)

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -8,6 +8,7 @@ const buildControl = (props) => (
     <div className ={classes.Label}>{props.label}</div>
         <button className={classes.Less} onClick={props.removed}disabled={props.disabled}>-</button>
         <button className={classes.More} onClick={props.added}>+</button>
+    
     </div>
 );
 
